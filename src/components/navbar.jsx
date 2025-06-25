b68888b31796a26492c509ebc7178b787d3569ef
leonardo.bocon <leonardo.bocon@manticore-labs.com>
2025-06-25 11:33:38 -0500

    cambios2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{Fragment} from 'react'
+import './navbarestilo.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Fragment>
+      <div className="container-fluid">
+        <div className="row Titulo">
+          <div className="col-sm-3 caja1 mb-5 mt-5">titulo</div>
+          <div className="col-md-6 caja2">titulo</div>
+          <div className="col-md-9 caja3">titulo</div>
+        </div>
+      </div>
+    </Fragment>
   )
 }
 
