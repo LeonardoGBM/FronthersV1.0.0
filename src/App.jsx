57740b1e8394a429e0600e85bf8208b1d8d16bb6
leonardo.bocon <leonardo.bocon@manticore-labs.com>
2025-06-25 11:06:08 -0500

    cambio1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import React from "react";
+import Navbar from './components/navbar'
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
