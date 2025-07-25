ddff95b9b8169d3fafc7cb38b79efeb8bb57d64b
leonardo.bocon <leonardo.bocon@manticore-labs.com>
2025-07-25 04:05:54 -0500

    responsive

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -33,7 +33,7 @@ const Inicio = () => {
       <div className="container-fluid">
         <div className="row fondo">
           <div className="col-12 descripcion">
-            <h1 className="titulo">Una pagína diseñada para tí</h1>
+            <h1 className="contenido">Una pagína diseñada para tí</h1>
             <p className="pt-5">
               Diseñamos sitios web modernos, visuales y optimizados para mostrar
               tu negocio en internet. <br />
@@ -45,7 +45,7 @@ const Inicio = () => {
             {/*Seccion de por que tener una pagina web*/}
 
             <div className="row">
-              <div className="col-md-6 text-start">
+              <div className="col-12 col-md-6 text-start">
                 <h2 className="contenido">¿Por qué tener una página web?</h2>
                 <p className="contenidop">
                   Tener una página web es fundamental en la actualidad porque
@@ -60,7 +60,7 @@ const Inicio = () => {
                   cada vez más digitalizado.
                 </p>
               </div>
-              <div className="col-md-6">
+              <div className="col-12 col-md-6">
                 <div className="col imagen">
                   <img src={img1} alt="imagen" />
                 </div>
@@ -68,13 +68,13 @@ const Inicio = () => {
             </div>
 
             <div className="row">
-              <div className="col-md-6">
+              <div className="col-12 col-md-6">
                 <div className="col imagen">
                   <img src={img2} alt="imagen" />
                 </div>
               </div>
 
-              <div className="col-md-6 text-start">
+              <div className="col-12 col-md-6 text-start">
                 <h2 className="contenido">
                   ¿Por qué confiar en nuestro servicio técnico?
                 </h2>
@@ -94,7 +94,7 @@ const Inicio = () => {
 
             {/*Servicion de licencias de windows */}
             <div className="row">
-              <div className="col-md-6 text-start">
+              <div className="col-12 col-md-6 text-start">
                 <h2 className="contenido">Licencias de windows</h2>
                 <p className="contenidop">
                   Ofrecemos licencias originales de Windows para que tu equipo
@@ -108,7 +108,7 @@ const Inicio = () => {
                   el proceso de activación e instalación.
                 </p>
               </div>
-              <div className="col-md-6">
+              <div className="col-12 col-md-6">
                 <div className="col-md imagen">
                   <img src={img3} alt="imagen" />
                 </div>
@@ -118,12 +118,12 @@ const Inicio = () => {
             {/*Asesoramiento */}
 
             <div className="row">
-              <div className="col-md-6">
+              <div className="col-12 col-md-6">
                 <div className="col-md imagen">
                   <img src={img4} alt="imagen" />
                 </div>
               </div>
-              <div className="col-md-6 text-start">
+              <div className="col-12 col-md-6 text-start">
                 <h2 className="contenido">
                   Asesoramiento y cotización de partes de computadora
                 </h2>
@@ -149,7 +149,7 @@ const Inicio = () => {
                   </h1>
                 </div>
               </div>
-              <div className="col-3">
+              <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div className="cardb">
                   <h5>
                     <b>PAQUETE BASICO</b>
@@ -161,7 +161,7 @@ const Inicio = () => {
                     <li>- 1 pestaña</li>
                     <li>- Responsive</li>
                   </ul>
-                  <div className="bot1 d-flex justify-content-center">
+                  <div className="bot d-flex justify-content-center">
                     <button className="btn btn-primary" onClick={() => paquete('Básico')}>
                       Solicitar Paquete
                     </button>
@@ -169,7 +169,7 @@ const Inicio = () => {
                 </div>
               </div>
 
-              <div className="col-3">
+              <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div className="cardn">
                   <h5>
                     <b>PAQUETE NORMAL</b>
@@ -182,7 +182,7 @@ const Inicio = () => {
                     <li>- Responsive</li>
                     <li>- Botones redes sociales</li>
                   </ul>
-                  <div className="bot2 d-flex justify-content-center">
+                  <div className="bot d-flex justify-content-center">
                     <button className="btn btn-primary" onClick={() => paquete('Normal')}>
                       Solicitar Paquete
                     </button>
@@ -190,7 +190,7 @@ const Inicio = () => {
                 </div>
               </div>
 
-              <div className="col-3">
+              <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div className="cardp">
                   <h5>
                     <b>PAQUETE PREMIUM</b>
