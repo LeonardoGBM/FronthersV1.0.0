d38653549f1450e54f714e692cc599458b84e9fb
jgcARAUJO <ventasjosegc@hotmail.com>
2025-07-30 01:39:49 -0500

    creacion-de-somos

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import Navbar from './components/navbar'
 import Footer from './components/footer'
 import Carrusel from './components/carrusel';
 import Inicio from './pages/Inicio'
-<<<<<<< HEAD
-import Contactanos from './pages/Contactanos'
+import Somos from './pages/Somos';
+import Contactos from './pages/contactos';
 
-=======
+
+
 import Servicio from './pages/Servicio';
->>>>>>> servicio
+
 function App() {
   return (
    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="servicio" element={<Servicio />} />
+        <Route path="somos" element={<Somos />} />
+        <Route path="contactos" element={<Contactos />} />
+        
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/contactos.jsx
@@ -0,0 +1,11 @@
+import React,{Fragment} from 'react'
+ const contactos = () => {
+  return (
+    <Fragment>  
+            <Navbar/> 
+            <h1>CONTACTOS</h1>
+            <Footer/>
+        </Fragment> 
+  )
+}
+export default contactos;
